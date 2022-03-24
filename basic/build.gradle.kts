c9c5939ee6eaf012ce5b9c73a96e2e04b882596f
Chen Chen <talent_prince@163.com>
2022-03-24 16:47:14 +0800

    ProducerRecord send

--- basic/build.gradle.kts
+++ basic/build.gradle.kts
@@ -9,4 +9,7 @@ repositories {
 
 dependencies {
     implementation(kotlin("stdlib"))
+    implementation("org.apache.kafka:kafka-clients:3.1.0")
+    implementation("org.slf4j:slf4j-api:1.8.0-beta4")
+    implementation("org.slf4j:slf4j-simple:1.8.0-beta4")
 }
---
+++ basic/src/main/kotlin/ProducerDemo.kt
@@ -0,0 +1,41 @@
+import org.apache.kafka.clients.producer.KafkaProducer
+import org.apache.kafka.clients.producer.ProducerConfig
+import org.apache.kafka.clients.producer.ProducerRecord
+import org.apache.kafka.common.serialization.StringSerializer
+import org.slf4j.LoggerFactory
+import java.time.Instant
+import java.time.LocalDateTime
+import java.time.ZoneId
+import java.time.ZoneOffset
+import java.util.*
+
+class ProducerDemo
+
+private val logger = LoggerFactory.getLogger(ProducerDemo::class.java.simpleName)
+
+fun main() {
+    val properties = Properties()
+    properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092")
+    properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer::class.java.name)
+    properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer::class.java.name)
+
+    (1..10).forEach {
+        val producer = KafkaProducer<String, String>(properties)
+        val record = ProducerRecord<String, String>("demo_java", "hello world")
+
+        producer.send(record) { metadata, exception ->
+            if (exception == null) {
+                logger.info(
+                    StringBuffer().appendLine("Topic: ${metadata.topic()}")
+                        .appendLine("Partition: ${metadata.partition()}").appendLine("Offset: ${metadata.offset()}")
+                        .appendLine("Timestamp: ${Instant.ofEpochMilli(metadata.timestamp()).atOffset(ZoneOffset.ofHours(8))}")
+                        .toString()
+                )
+            } else {
+                logger.error("Error while publishing")
+            }
+        }
+        producer.flush()
+        producer.close()
+    }
+}
